6a3f30566b839901b398fff08bad6f0a9ad0afa2
Ahmad-Hafizh <ahaaafizh@gmail.com>
2024-11-09 20:40:24 +0700

    first time upload - landing-page

--- src/views/home/Testimonial.tsx
+++ src/views/home/Testimonial.tsx
@@ -28,61 +28,9 @@ const Testimonial = () => {
                 <p>Chengdu, China</p>
               </div>
               <p>
-                &quot;Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                &#34;Lorem, ipsum dolor sit amet consectetur adipisicing elit.
                 Enim aliquam suscipit explicabo inventore. Quaerat corporis
-                ratione asperiores a aspernatur debitis.&quot;
-              </p>
-            </div>
-          </CarouselItem>
-          <CarouselItem className="md:basis-1/2 lg:basis-1/3">
-            <div className="flex h-[200px] w-full flex-col gap-4 rounded-xl border-2 bg-white p-4">
-              <div className="">
-                <p className="text-xl font-semibold">John Doe</p>
-                <p>Chengdu, China</p>
-              </div>
-              <p>
-                &quot;Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                Enim aliquam suscipit explicabo inventore. Quaerat corporis
-                ratione asperiores a aspernatur debitis.&quot;
-              </p>
-            </div>
-          </CarouselItem>
-          <CarouselItem className="md:basis-1/2 lg:basis-1/3">
-            <div className="flex h-[200px] w-full flex-col gap-4 rounded-xl border-2 bg-white p-4">
-              <div className="">
-                <p className="text-xl font-semibold">John Doe</p>
-                <p>Chengdu, China</p>
-              </div>
-              <p>
-                &quot;Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                Enim aliquam suscipit explicabo inventore. Quaerat corporis
-                ratione asperiores a aspernatur debitis.&quot;
-              </p>
-            </div>
-          </CarouselItem>
-          <CarouselItem className="md:basis-1/2 lg:basis-1/3">
-            <div className="flex h-[200px] w-full flex-col gap-4 rounded-xl border-2 bg-white p-4">
-              <div className="">
-                <p className="text-xl font-semibold">John Doe</p>
-                <p>Chengdu, China</p>
-              </div>
-              <p>
-                &quot;Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                Enim aliquam suscipit explicabo inventore. Quaerat corporis
-                ratione asperiores a aspernatur debitis.&quot;
-              </p>
-            </div>
-          </CarouselItem>
-          <CarouselItem className="md:basis-1/2 lg:basis-1/3">
-            <div className="flex h-[200px] w-full flex-col gap-4 rounded-xl border-2 bg-white p-4">
-              <div className="">
-                <p className="text-xl font-semibold">John Doe</p>
-                <p>Chengdu, China</p>
-              </div>
-              <p>
-                &quot;Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                Enim aliquam suscipit explicabo inventore. Quaerat corporis
-                ratione asperiores a aspernatur debitis.&quot;
+                ratione asperiores a aspernatur debitis.&#34;
               </p>
             </div>
           </CarouselItem>
