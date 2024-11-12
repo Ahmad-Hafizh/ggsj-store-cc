225a8c33ceff1920f1fe9cd4130e43d1dfc03a68
Ahmad-Hafizh <ahaaafizh@gmail.com>
2024-11-12 08:21:41 +0700

    product type fix

--- src/lib/redux/reducers/productsSlice.ts
+++ src/lib/redux/reducers/productsSlice.ts
@@ -1,11 +1,26 @@
-import { TypeProductGgsjAsset } from "@/types/product.type";
 import { createSlice } from "@reduxjs/toolkit";
 
 interface IProduct {
   fields: {
     title: string;
     slug: string;
-    cover?: TypeProductGgsjAsset;
+    cover?: {
+      sys: { id: string };
+      fields: {
+        file: {
+          url: string;
+          details?: {
+            size: number;
+            image: {
+              width: number;
+              height: number;
+            };
+          };
+          fileName?: string;
+          contentType?: string;
+        };
+      };
+    };
     category?: string[];
   };
 }
